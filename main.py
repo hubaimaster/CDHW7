3b39eb069db3cff5e1814c04acc8fbcdacb3b181
kimchanghwan <hubaimaster@gmail.com>
2021-10-03 20:10:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 if __name__ == '__main__':
     print('Hello world!')
-    print('Update in local C')
-    print('Update in local B')
     print('Update in local A')
+    print('Update in local B')
+    print('Update in local C')
     print("Update in leader")
